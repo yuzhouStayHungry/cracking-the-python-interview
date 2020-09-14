666b48064f6ac0db44a595734f7e111ab45b4a4d
yuzhou_1su <yuzhou_1shu@163.com>
2020-09-14 09:32:18 +0800

    增加单链表代码

---
+++ Data Structure And Algorithms/Tree/BinaryTree.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Time      : 2020-09-12 23:01
+# @Author    : yuzhou_1su
+# @ContactMe : www.yuzhou_1su@163.com
+# @File      : BinaryTree.py
+# @Software  : PyCharm
+
+
+class BinaryTreeNode:
+
+    def __init__(self, data=None, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
